dec23ace04f400796a6140654eb3393ff0f79226
Arty <joe@email.com>
2022-08-21 20:12:33 +0200

    initial commit

---
+++ tests/hello-world.component.test.js
@@ -0,0 +1,26 @@
+import {html, fixture, expect} from "@open-wc/testing";
+import {HelloWorldComponent} from "../src/components/hello-world.component";
+
+import "../src/components/hello-world.component";
+
+describe("HelloWorldComponent", () => {
+    it("should be a constructor", async () => {
+        const element = new HelloWorldComponent();
+
+        expect(element).to.be.an("HTMLElement");
+    });
+
+    it("should be constructable via HTML", async () => {
+        const element = await fixture(html`<hello-world></hello-world>`);
+
+        expect(element).to.be.an("HTMLElement");
+    });
+
+    it("should contain a span with th text 'Hello World!'", async () => {
+        const element = await fixture(html`<hello-world></hello-world>`);
+
+        const span = element.shadowRoot.querySelector("span");
+
+        expect(span.textContent).to.equal("Hello World!");
+    });
+})
