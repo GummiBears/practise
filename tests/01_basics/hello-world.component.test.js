a43a01cacff3ff49044145e0ed37f9ed2e8d624f
Arty <joe@email.com>
2022-08-22 19:16:18 +0200

    feat: add more practise tests

--- tests/01_basics/hello-world.component.test.js
+++ tests/01_basics/hello-world.component.test.js
@@ -18,5 +18,19 @@ describe("01: Basics", () => {
 
       expect(span.textContent).to.equal("Hello World!");
     });
+
+    it("should accept an initial attribute named 'foo'", async () => {
+      const element = await fixture(html`<hello-world foo="bar"></hello-world>`);
+
+      expect(element.foo).to.equal("bar");
+    });
+
+    it("should reflect a property change on 'foo' via script in its html attribute", async () => {
+      const element = await fixture(html`<hello-world></hello-world>`);
+
+      element.foo = "bar";
+
+      expect(element.getAttribute("foo")).to.equal("bar");
+    });
   });
 });
